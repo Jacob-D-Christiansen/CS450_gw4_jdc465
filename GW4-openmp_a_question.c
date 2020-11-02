5f850f4c4fa0444893fb3465b060704991198bbb
Jacob <Jacob.D.Christiansen@gmail.com>
2020-11-02 16:11:13 -0700

    code is done?

--- GW4-openmp_a_question.c
+++ GW4-openmp_a_question.c
@@ -6,7 +6,7 @@
 #define N 40000
 #define THRESHOLD 1.0
 
-#define NTHREADS 4
+#define NTHREADS 1
 
 void populatedata(double * x, double * y);
 double expodist(double lambda);
@@ -21,23 +21,38 @@ int main(int argc, char **argv) {
   double * x=(double *)malloc(sizeof(double)*N);
   double * y=(double *)malloc(sizeof(double)*N);
   int * z=(int *)malloc(sizeof(int)*N);
-  printf("\nAllocated %f GiB", ((sizeof(double)*N*2.0)+(sizeof(int)*N*1.0))/(1024*1024*1024.0));
+  double runningSum = 0.0;
+  printf("\nAllocated %f GiB\n", ((sizeof(double)*N*2.0)+(sizeof(int)*N*1.0))/(1024*1024*1024.0));
   populatedata(x, y);
 
   //Write code here
-
+  double tstart = omp_get_wtime();
+  
+  #pragma omp parrallel shared(z, x, y, N) private(i)
+  for(int i = 0; i < N; i++)
+  {
+    z[i] = (int)computedistance(&x[i], &y[i]);
+  }
+  
+  double tend = omp_get_wtime();
+  
+  for(int i = 0; i < N; i++)
+  {
+    runningSum += z[i];
+  }
 
 
   free(x);
   free(y);
   free(z);
+  printf("Total Time: %f\n", tend-tstart);
+  printf("z sum: %f\n", runningSum);
   printf("\n\n");
   return 0;
 }
 
 double computedistance(double * a, double * b)
 {
-  
   return(fabs((*a)-(*b)));
 }
 
@@ -52,9 +67,6 @@ void populatedata(double * x, double * y)
     
   }
 }
-
-
-
 
 double expodist(double lambda)
 {
